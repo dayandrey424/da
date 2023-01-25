492377673e03bbe3cebea1877bd7c15121ee3148
dayandrey424 <123003008+dayandrey424@users.noreply.github.com>
2023-01-25 19:06:41 +0300

    Add files via upload

---
+++ my_app.py
@@ -0,0 +1,18 @@
+from PyQt5.QtCore import Qt, QTimer, QTime, QLocale
+from PyQt5.QtGui import QDoubleValidator, QIntValidator, QFont
+from PyQt5.QtWidgets import (
+        QApplication, QWidget,
+        QHBoxLayout, QVBoxLayout, QGridLayout,
+        QGroupBox, QRadioButton,
+        QPushButton, QLabel, QListWidget,QLineEdit)
+
+from instr import *
+from second_win import *
+
+class MainWin(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.
+        self.
+        self.
+        self. 
